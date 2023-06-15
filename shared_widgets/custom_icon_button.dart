0fb66ea643c62ee6425cd980c5ce4fe88970468f
MiladKhalighi <milad.kh4lighi@gmail.com>
2023-06-15 11:53:32 -0400

    update and modify custom icon button widget

--- shared_widgets/custom_icon_button.dart
+++ shared_widgets/custom_icon_button.dart
@@ -1,5 +1,8 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:personal_webpage/controller/custom_icon_button_controller.dart';
 
 import '../components/app_colors.dart';
 import '../components/app_dimentions.dart';
@@ -15,22 +18,33 @@ class CustomIconButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      //borderRadius: BorderRadius.circular(50),
-      splashColor: AppColors.onSecondary,
-      onTap: onTap,
-      child: Container(
-          padding: EdgeInsets.all(AppDimentions.small),
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.white,
-          ),
-          child: SvgPicture.asset(
-            iconPath,
-            width: 24,
-            height: 24,
-            fit: BoxFit.cover,
-          )),
+    var controller = Get.put(CustomIconButtonController(),tag: UniqueKey().toString());
+    return Obx(
+      ()=> InkWell(
+        splashColor: AppColors.onSecondary,
+        highlightColor: AppColors.onSecondary,
+        onTap: onTap,
+        onHover: (value) => controller.hovering = value,
+        child: Container(
+            padding: EdgeInsets.all(AppDimentions.small),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: controller.hovering ? AppColors.onSecondary : Colors.white,
+            ),
+            child: controller.hovering ? ZoomIn(
+              child: SvgPicture.asset(
+                iconPath,
+                width: 24,
+                height: 24,
+                fit: BoxFit.cover,
+              ),
+            ): SvgPicture.asset(
+                iconPath,
+                width: 24,
+                height: 24,
+                fit: BoxFit.cover,
+              ),),
+      ),
     );
   }
 }
